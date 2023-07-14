00269eb4c8874151d5ea87f604543ed63d0a516a
AjayKumar-4126 <139580487+AjayKumar-4126@users.noreply.github.com>
2023-07-15 00:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.title("Movie Recommendatation System")
-movie_df=pickle.load(open("drive/MyDrive/movie_recommendation/movie_recm.pkl","rb"))
+movie_df=pickle.load(open("movie_recm.pkl","rb"))
 similarity=pickle.load(open("similarity.pkl","rb"))
 list_movie=np.array(movie_df["title"])
 option = st.selectbox(
